810876219d478bb9fc36cc52ab8136685d366e2c
HarveyYifanLi <liyifan201209@gmail.com>
2020-07-18 16:32:53 -0700

    Update alphanumerics.js

--- alphanumerics.js
+++ alphanumerics.js
@@ -1,1 +1,14 @@
+// function that checks if a string/char is alphanumeric
+// by using RegExp instance and method
+function isAlphanumeric(str){
+  return /^[a-zA-Z0-9]+$/.test(str);
+}
 
+// function that deletes all spaces from a string using RegExp
+function deleteSpaces(str){
+  return str.replace(/\s+/g, ''); // with 'g' added, will replace all matches globally, otherwise, only replace the first matched instance
+}
+// function that deletes all spaces from a string using array method
+function deleteSpaces(str){
+  return str.split('').forEach(char => char.trim()).join();
+}
